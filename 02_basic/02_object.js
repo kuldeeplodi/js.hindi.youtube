898b199ed5500bf7cc522dae7c122d3b4ee2579c
kuldeeplodi <144382128+kuldeeplodi@users.noreply.github.com>
2024-09-05 04:02:06 +0000

    object file

--- 02_basic/02_object.js
+++ 02_basic/02_object.js
@@ -12,8 +12,8 @@ const Jsuser={
     email:"kuldeeplodhi@gmail.com"
 }
 
-console.log(Jsuser["city"])
-console.log( Jsuser[mysym])
+// console.log(Jsuser["city"])
+// console.log( Jsuser[mysym])
 
 Jsuser.name="kumkum"
 
@@ -29,8 +29,8 @@ Jsuser.greetingtwo=function(){
     console.log(`hello JS user ${this.name}`)
 }
 
-console.log(Jsuser.greeting())
-console.log(Jsuser.greetingtwo())
+// console.log(Jsuser.greeting())
+// console.log(Jsuser.greetingtwo())
 
 /*****************Object part 2****************************************/
 
@@ -40,7 +40,49 @@ const tinderUser={}//non singleton object
 tinderUser.id="232abd"
 tinderUser.name="kuldeep"
 tinderUser.email="kuldeep@gmail.com"
-console.log(tinderUser)
 
 
 
+// const IGuser={
+//     Id:34234,
+//     fullname:{
+//         username:{
+//             firstname:"kuldeep",
+//             lastname:"lodhi"
+//         }
+//     }
+// }
+
+// console.log(IGuser.fullname.username.lastname)
+
+
+// const obj1={1:"a",2:"b"}
+// const obj2={3:"d",4:"c"}
+// const obj3={8:"o",6:"p"}
+
+// const obj4={...obj1,...obj2,...obj3}
+// console.log(obj4)
+
+// console.log(tinderUser)
+
+// console.log(Object.keys(tinderUser))//return array of keys
+// console.log(Object.values(tinderUser))// return array of values
+// console.log(Object.entries(tinderUser))//[[ 'id', '232abd' ],[ 'name', 'kuldeep' ],[ 'email', 'kuldeep@gmail.com' ]]
+
+
+// console.log(tinderUser.hasOwnProperty("name"))//true
+
+
+//de-structuring of object
+
+const course={
+    coursename:"javascript",
+    price:"1999",
+    courseInstructor:"kuldeep"
+
+}
+
+//course.courseInstructor
+
+const {courseInstructor}=course
+console.log(courseInstructor)
