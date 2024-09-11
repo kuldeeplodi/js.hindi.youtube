9c346f753a06ea95d93424eaaf66c275c9d659f6
kuldeeplodi <144382128+kuldeeplodi@users.noreply.github.com>
2024-09-11 17:11:46 +0000

    for and while loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,35 @@
+//for
+// const array=[3,5,2,4,7,9]
+// for (let index = 0; index < array.length; index ++) {
+//     const element = array[index];
+//     console.log(element)  
+// }
+//console.log(element)
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(`table of ${i}`)
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i}*${j}=${i*j}`)
+        
+//     }
+    
+// }
+
+
+//break and continue
+
+// for (let i = 0; i < 10; i++) {
+//    if(i==5){
+//     console.log("detected 5")
+//     break
+//    }
+//     console.log(i)
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     if(i==5){
+//      console.log("detected 5")
+//      continue
+//     }
+//      console.log(i)
+//  }
---
+++ 05_iteration/two.js
@@ -0,0 +1,22 @@
+//while
+// let index=0
+// while(index<10){
+//     console.log(index)
+//     index=index+3
+// }
+
+// let array=["ironman","thor","spiderman","captainAmerica"]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element)
+// }
+
+
+//do while
+
+// let score=1
+
+// do{
+//     console.log(`score is ${score}`)
+//     score++
+// }while(score <= 10)
