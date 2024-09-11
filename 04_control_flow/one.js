c6bb8acf04d1f62ac75f8a5845d56e01d4a1617d
kuldeeplodi <144382128+kuldeeplodi@users.noreply.github.com>
2024-09-11 16:35:44 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,78 @@
+//if
+ 
+// if(true){
+
+// }
+
+//else if
+
+// if(){
+
+// }else if(){
+
+// }
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month=3
+// switch (month) {
+//     case 1:
+//         console.log("january")
+//         break;
+//     case 2:
+//         console.log("feb")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+
+//     default:
+//         console.log("month not found")
+//         break;
+// }
+
+// const useremail=[]
+
+// if(useremail){
+//     console.log("got user email")
+// }else{
+//     console.log("don't have user email")
+// }
+
+
+//falsy values
+//false , 0,-0,BigInt 0n,"",null, undefined,NaN
+
+//truthy values
+//"0", 'false' ,  " " ,[],{},function(){}
+
+// if(useremail.length===0){
+//     console.log("Array is empyt")
+// }
+
+// const empytobject={}
+// if(object.keys(empytobject).length===0){
+//     console.log("object is empyt")
+// }
+
+//Nullish coalescing operator (??): null undefined
+
+// let val1
+//val1=5??10
+//val1=null??10
+//val1=undefined??10
+//val1=null??10??20
+
+
+// console.log(val1)
+
+
+//ternary operator
+// conditon ? true : false
