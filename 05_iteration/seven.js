594f7fdb54e542ffb698433d5dfde3e1ae75841e
kuldeep lodhi <144382128+kuldeeplodi@users.noreply.github.com>
2024-09-14 17:33:43 +0000

    reduce method

--- 05_iteration/seven.js
+++ 05_iteration/seven.js
@@ -4,3 +4,42 @@ const newnum=mynum.map((num)=>{
     return num+10
 })
 console.log(newnum)
+
+
+//reduce((accumalater,currentvalue)=>{accumalater+currentvalue},initalvalue)
+
+const total=mynum.reduce((acc,currval)=>{
+    console.log(`acc:${acc} and currval:${currval}`)
+    return acc+currval
+},0)
+
+console.log(total)
+
+
+const shoppingCart=[
+    {
+    itemName:"java course",
+    price:1000
+},
+    {
+    itemName:"c++ course",
+    price:1030
+},
+    {
+    itemName:"c course",
+    price:1200
+},
+    {
+    itemName:"js course",
+    price:10282
+},
+    {
+    itemName:"python course",
+    price:1342
+}
+
+]
+
+const total_bill=shoppingCart.reduce((acc,item)=>{return acc+item.price},0)
+
+console.log(`total bill is :${total_bill}`)
